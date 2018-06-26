747807b24a31bf9a52d6922f990f30460dbc4d6d
Scarlett Patton <scarwars05@gmail.com>
2018-06-26 15:23:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 $(document).ready(function (){
 });
+
+function searchRep() {
+  const searchTerms = $('#searchTerms').val();
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`),
+  data => {
+    $('#results').html(renderSearchResults(data));
+  };
+}
+
+function showCommits() {
+  
+}
+
+function displayError() {
+  
+}
